4ddbbab39fd50e3f5d39ddb45b158bd05f03bc0e
Jozexo <josegollitohernandezr@gmail.com>
2026-04-14 12:46:21 -0400

    fix(vite.config): corregir la ruta base a "/Binary-Brain/" en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/binary-brain/",
+  base: "/Binary-Brain/",
   server: {
     host: true,
     watch: {
